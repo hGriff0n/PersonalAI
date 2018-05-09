a00c3a64d2fdf048f1d8510525597003c62fd0a0
Grayson Hooper <ghooper96@gmail.com>
2018-05-08 21:51:16 -0400

    Add Asynchronous Speech Recognition

--- speech.py
+++ speech.py
@@ -2,30 +2,50 @@
 
 # NOTE: Requires PyAudio for use of Microphone
 
+import asyncio
+
 import speech_recognition as sr
 import time
 
-def callback(recognizer, audio):
-    try:
-        print("Google thinks you said \"" + recognizer.recognize_google(audio) + "\"")
-    except sr.UnknownValueError:
-        print("Google could not understand the audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google: {0}".format(e))
+# NOTE: SpeechRecognition currently doesn't natievly support asyncio
+# This is a simple workaround to enable running of events while listening
+# Taken from: https://github.com/Uberi/speech_recognition/issues/137
+async def listen_async(self, source):
+    import threading
+    result_future = asyncio.Future()
 
-r = sr.Recognizer()
-m = sr.Microphone()
-with m as source:
-    r.adjust_for_ambient_noise(source)
+    def threaded_listen():
+        with source as s:
+            try:
+                audio = self.listen(s)
+                loop.call_soon_threadsafe(result_future.set_result, audio)
+            except Exception as e:
+                loop.call_soon_threadsafe(result_future.set_exception, e)
 
-# 'stop_listening' is a function that stops background listening when called
-stop_listening = r.listen_in_background(m, callback)
-print("You can start talking now")
+    listener_thread = threading.Thread(target=threaded_listen)
+    listener_thread.daemon = True
+    listener_thread.start()
+    return await result_future
 
-for _ in range(50):
-    time.sleep(0.1)
+async def run():
+    r = sr.Recognizer()
+    m = sr.Microphone()
+    r.adjust_for_ambient_noise(m)
 
-stop_listening()
+    print("You can start talking now\n")
+
+    while True:
+        audio_data = await listen_async(r, m)
+        query = r.recognize_google(audio_data)
+        print("> {}".format(query))
+        if query == "quit":
+            break
+
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(run())
+finally:
+    loop.close()
 
 # API Documentation: https://github.com/Uberi/speech_recognition/blob/master/reference/library-reference.rst
 # https://realpython.com/python-speech-recognition/
