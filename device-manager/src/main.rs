2da798c82d0161c84c7676eea810eea23451c8f2
Grayson Hooper <ghooper96@gmail.com>
2018-05-28 08:07:03 -0400

    Add Cancellation and Message Modification to Echo Server
    Finally managed to coerce tokio to allow for modifying the returned message inside of the write action (note: this may not be fully necessary in the full implementation)
    Figured out where to place the cancel future to enable oneshot echo communication

--- device-manager/src/main.rs
+++ device-manager/src/main.rs
@@ -1,4 +1,4 @@
-// extern crate futures;
+extern crate futures;
 extern crate tokio;
 // extern crate tokio_core;
 extern crate tokio_io;
@@ -23,6 +23,9 @@ use serde_json::Value;
 use tokio_serde_json::*;
 
 // Adapt this with the server to enable easy two-way communication
+    // I don't think I'll need to have an infinite back-and-forth communication
+        // Receive play, send to a different process (I think do need to send a "done" message though)
+        // Only reason would be to maintain state between negotiated communications
     // Send events back and forth, modify behavior based on the event
 // Once I have this implementation done, develop a python bridge package
 // Transition over to getting the modalities to work on the individual channel
@@ -34,27 +37,47 @@ fn main() {
     let addr = "127.0.0.1:6142".parse::<SocketAddr>().unwrap();
     let listener = TcpListener::bind(&addr).unwrap();
 
+    // TODO: See if I can write this similar to the server implementation
     let server = listener.incoming().for_each(|conn| {
         // Split the connection into reader and writer
+        // Maddeningly, `conn.split` produces `(reader, writer)`
         let (writer, reader) = length_delimited::Framed::new(conn).split();
         let writer = WriteJson::<_, Value>::new(writer);
         let reader = ReadJson::<_, Value>::new(reader);
 
         // Setup the stop channel
         let (tx, rx) = std::sync::mpsc::channel();
-        let cancel = comm::Canceller{ rx: rx };
+        let cancel = comm::FutureChannel::new(rx);
 
-        // Produce the action
-        let action = reader.map(move |msg| {
-                tx.send(()).unwrap();
+        // Setup the communication channel
+        let (sink, source) = std::sync::mpsc::channel::<Value>();
+        let source = comm::FutureChannel::new(source);
+
+        // Define the reader action
+        let read_action = reader.for_each(move |msg| {
                 println!("GOT: {:?}", msg);
-                msg
-            })
-            .forward(writer)
-            .select2(cancel)
-            .map(|_| {})
-            .map_err(|err| println!("error"));          // TODO: Why can't I print the error ?
+                Ok(sink.send(msg).unwrap())
+            });
 
+        // Define the writer action
+        let write_action = writer.send_all(
+            source.transform(move |mut msg| {
+                msg["resp"] = json!("World");
+
+                // Temporarily restrict the communication to a single call-response
+                tx.send(()).unwrap();
+
+                msg
+            }));
+
+        // Combine the actions into one "packet" for registration with tokio
+        let action = read_action
+            .select2(write_action)
+            .select2(cancel)                                // NOTE: This needs to come last in order for it to work
+            .map(|_| {})
+            .map_err(|_| ());                               // NOTE: I'm ignoring all errors for now
+
+        // Finally spawn the connection
         tokio::spawn(action);
         Ok(())
     })
--- server/src/main.rs
+++ server/src/main.rs
@@ -37,23 +37,6 @@ fn main() {
                     })
                 })
         })
-    // let client = TcpStream::connect(&addr)
-    //     .and_then(|conn| {\
-    //         // let (writer, reader) = length_delimited::Framed::new(conn).split();
-    //         // let writer = WriteJson::new(writer);
-    //         // let _reader = ReadJson::new(reader);
-
-    //         // writer.send(json!({ "text": "Hello" }))
-    //             // .and_then(|conn| )
-    //         let framed = conn.framed(LinesCodec::new());
-    //         framed.send("Hello!".to_string())
-    //             .and_then(|conn| {
-    //                 conn.for_each(|line| {
-    //                     println!("Received line {}", line);
-    //                     Ok(())
-    //                 })
-    //             })
-    //     })
         .map_err(|err| println!("Stream error: {:?}", err));
 
     tokio::run(client)
