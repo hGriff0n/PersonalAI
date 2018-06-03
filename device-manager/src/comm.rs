58916a5ea0212cdc411eab27242a55f67dd3705d
Grayson Hooper <ghooper96@gmail.com>
2018-06-03 18:08:39 -0400

    Got Message Fanout Working
    Finally figured out how to send messages to clients other than the sender without throttling network traffic
    Dropped futures crate to 0.1.21 due to API requirements
    Removed Stream impl from FutureChannel

--- device-manager/src/comm.rs
+++ device-manager/src/comm.rs
@@ -1,11 +1,9 @@
 extern crate tokio;
 
 use std;
-use std::io;
 use std::sync::mpsc;
 
-use tokio::prelude::{Async, Future, Stream};
-use tokio::prelude::stream::Map;
+use tokio::prelude::{Async, Future};
 
 pub struct FutureChannel<T> {
     rx: mpsc::Receiver<T>,
@@ -15,10 +13,6 @@ impl<T: std::fmt::Debug> FutureChannel<T> {
     pub fn new(rx: mpsc::Receiver<T>) -> Self {
         Self{ rx: rx }
     }
-
-    pub fn transform<U, F>(self, f: F) -> Map<Self, F> where F: FnMut(T) -> U {
-        Stream::map(self, f)
-    }
 }
 
 impl<T: std::fmt::Debug> Future for FutureChannel<T> {
@@ -26,25 +20,8 @@ impl<T: std::fmt::Debug> Future for FutureChannel<T> {
     type Error = mpsc::RecvError;
 
     fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
-        let val = self.rx.try_recv();
-        println!("poll {:?}", val);
-        match val {
+        match self.rx.try_recv() {
             Ok(val) => Ok(Async::Ready(val)),
-            Err(_) => Ok(Async::NotReady)
-        }
-    }
-}
-
-
-impl<T: std::fmt::Debug> Stream for FutureChannel<T> {
-    type Item = T;
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
-        let val = self.rx.try_recv();
-        println!("poll {:?}", val);
-        match val {
-            Ok(val) => Ok(Async::Ready(Some(val))),
             Err(_) => Ok(Async::NotReady)
         }
     }
--- device-manager/src/main.rs
+++ device-manager/src/main.rs
@@ -2,6 +2,7 @@ extern crate tokio;
 extern crate tokio_io;
 extern crate tokio_serde_json;
 #[macro_use] extern crate serde_json;
+extern crate futures;
 
 mod comm;
 
@@ -20,9 +21,12 @@ use tokio_io::codec::length_delimited::Framed;
 use serde_json::Value;
 use tokio_serde_json::*;
 
-// Test whether "forwarding" messages works
-    // Setup "server state" structures for the handle functions
-// Figure out how to make this "service" capable of acting as a client and a server (of forcing a connection to another running instance)
+// Get working cross-device communication (move away from home ip)
+    // Figure out how to implement discovery so I don't have to hardcode paths
+    // Test whether "forwarding" messages works
+        // Setup "server state" structures for the handle functions
+    // Setup device "name" registration, "to" field, and other setup handling
+// Figure out how to use futures 0.2.1
 // Once I have this implementation done, develop a python bridge package
 // Transition over to getting the modalities to work on the individual channel
 // Change the dispatch to a separate app, queried by this
@@ -46,41 +50,33 @@ fn main() {
         let cancel = comm::FutureChannel::new(rx);
 
         // Setup the communication channel
-        let (sink, source) = mpsc::channel::<Value>();
-        let source = comm::FutureChannel::new(source);
+        let (sink, source) = futures::sync::mpsc::unbounded();
 
         // Register the connection
         let addr = conn.peer_addr().unwrap();
-        connections.lock().unwrap().insert(addr, (tx, sink));
+        {
+            connections.lock().unwrap().insert(addr, (tx, sink));
+        }
 
         // Split the connection into reader and writer
         // Maddeningly, `conn.split` produces `(reader, writer)`
         let (writer, reader) = Framed::new(conn).split();
-        let writer = WriteJson::<_, Value>::new(writer);
+        let writer = WriteJson::<_, Value>::new(writer).sink_map_err(|_| ());
         let reader = ReadJson::<_, Value>::new(reader);
 
         // Define the reader action
         let read_conns = connections.clone();
-        let read_action = reader
-            .for_each(move |msg| handle_request(msg, &read_conns, &addr));
+        let read_action = reader.for_each(move |msg| {
+                handle_request(msg, &read_conns, &addr)
+            });
 
         // Define the writer action
         let write_conns = connections.clone();
-
-        // This doesn't actually work for sending to a different client (only for echoing)
-            // This is only for "sparse" connections (if I'm constantly communicating then some get through)
-                // I have to always be sending some messages to the client for some other messages to be sent
-            // This actually looks like some sort of deadlock
-                // Without the constant communication, something gets stuck and we don't precede
-                // But what? None of the source fold stuff gets run after the communication ends
-            // The actions are both still active when the halt occurs
-            // Changing the sender to a `SyncSender` doesn't solve it either
-        // let write_action = source
-        //     .transform(move |msg| handle_response(msg, &write_conns, &addr))
-        //     .forward(writer);
-        let write_action = writer
-            .send_all(source
-                .transform(move |msg| handle_response(msg, &write_conns, &addr)));
+        let write_action = source
+            .map(move |msg| handle_response(msg, &write_conns, &addr))
+            .forward(writer)
+            .map(|_| ())
+            .map_err(|_| ());
 
         // Combine the actions into one "packet" for registration with tokio
         let close_conns = connections.clone();
@@ -88,7 +84,6 @@ fn main() {
             .select2(write_action)
             .select2(cancel)                                // NOTE: This needs to come last in order for it to work
             .map(move |_| {                                 // Remove the connection data from system state
-                println!("Closing");
                 close_conns.lock().unwrap().remove(&addr);
             })
             .map_err(|_| ());                               // NOTE: I'm ignoring all errors for now
@@ -108,47 +103,27 @@ fn main() {
 // API Documentation:
 //  tokio-serde-json: https://github.com/carllerche/tokio-serde-json
 
-type Connections = Arc<Mutex<HashMap<SocketAddr, (mpsc::Sender<()>, mpsc::Sender<Value>)>>>;
+type Connections = Arc<Mutex<HashMap<SocketAddr, (mpsc::Sender<()>, futures::sync::mpsc::UnboundedSender<Value>)>>>;
 
 #[allow(unused_mut)]
-fn handle_request(msg: Value, conns: &Connections, addr: &SocketAddr) -> Result<(), tokio::io::Error> {
-    // println!("GOT: {:?}", msg);
-    static mut count: i32 = 0;
-
-    // I can get communication to occur if I spawn them fast enough
-    // if unsafe { count < 300 } {
-    //     unsafe { count += 1; }
-    //     conns.lock().unwrap()[addr].1.clone().send(msg).expect("Failed to send");
-    //     return Ok(())
-    // }
+fn handle_request(mut msg: Value, conns: &Connections, addr: &SocketAddr) -> Result<(), tokio::io::Error> {
+    println!("GOT: {:?}", msg);
 
     let mut conns = conns.lock().unwrap();
-    // let iter = conns.iter_mut();
-        // .filter(|&(&k, _)| k != *addr);
-    //     .map(|(_, v)| v);
+    let iter = conns.iter_mut()
+        .filter(|&(&k, _)| k != *addr);
 
-
-    // The receiver says it's empty iff I add the address check
-    for (to, (_, sink)) in conns.iter() {
-        if to != addr
-        {
-            println!("Sending to {:?}", to);
-        // }
-            sink.clone()
-                .send(json!({ "resp": *addr }))
-                .expect("Failed to send");
-            // println!("Sent to {:?}", to);
-        }
+    for (_to, (_, sink)) in iter {
+        sink.clone()
+            .unbounded_send(json!({ "from": *addr }))
+            .expect("Failed to send");
     }
-
-    // conns[addr].1.send(msg).expect("Failed to send message");
 
     Ok(())
 }
 
-fn handle_response(msg: Value, conns: &Connections, addr: &SocketAddr) -> Value {
-    // msg["resp"] = json!("World");
-    println!("Sending message");
+fn handle_response(mut msg: Value, conns: &Connections, addr: &SocketAddr) -> Value {
+    msg["resp"] = json!("World");
 
     if let Some(action) = msg.get("action") {
         if action == "quit" {
