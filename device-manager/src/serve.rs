9d38b78b3500f954e3654c7285f136eb72e3fe67
Grayson Hooper <ghooper96@gmail.com>
2018-09-28 21:27:35 -0400

    Split Server Futures Into Separate Function
    Splits out the creation of the server futures into a separate function, allowing for greater composablity with future systems (as the tokio running code isn't tied to any one location).

--- device-manager/src/serve.rs
+++ device-manager/src/serve.rs
@@ -2,65 +2,67 @@
 use std::net::SocketAddr;
 use std::sync::mpsc;
 
+use std::io;
+use std::result::Result;
+
 use tokio;
 use tokio::prelude::*;
 use tokio::net::{ TcpListener, TcpStream };
+
+use futures;
 
 use server::comm;
 use server::spawn::spawn_connection;
 
 use super::device::DeviceManager;
 
+// TODO: What types do I need to unify this
+pub fn create_server(device: DeviceManager, addr: SocketAddr, parent: Option<SocketAddr>) -> Box<dyn futures::Future<Item=(), Error=()> + Send> {
+    let ai_device = device.clone();
+    let server = TcpListener::bind(&addr)
+        .unwrap()
+        .incoming()
+        .for_each(move |conn| Ok(spawn_connection(conn, device.clone())))
+        .map_err(|err| error!("Server Error: {:?}", err));
+
+    if let Some(paddr) = parent {
+        info!("Initializing web-node device-manager");
+        info!("Connecting to parent device at {}", paddr);
+
+        let client = TcpStream::connect(&paddr)
+            .and_then(move |conn| Ok(spawn_connection(conn, ai_device)))
+            .map_err(|err| { error!("Client error: {:?}", err) });
+
+        Box::new(server.join(client).map(|_| ()))
+
+    } else {
+        info!("Initializing standalone device-manager");
+        Box::new(server)
+    }
+}
 
 // TODO: This should allow us to shut-down the server from within, but it doesn't. Why?
 pub fn serve(addr: SocketAddr, parent: Option<SocketAddr>) {
+    // NOTE: Everything in this function will likely be moved to some different module (or even main)
+    // This allows us to more generally bring in new capabilities/etc. as we no longer requre the server code to launch tokio
     // Setup stop communication
     let (tx, cancel) = mpsc::channel();
     let cancel = comm::FutureChannel::new(cancel);
 
-    // Create manager
     let manager = DeviceManager::new(parent, tx.clone());
-    let ai_client = manager.clone();
-
 
     // TODO: Spawn any timer threads (eg. Seshat indexing)
+    // NOTE: We will likely move this to a different file (or even main)
 
+    let server = create_server(manager.clone(), addr, parent);
 
-    // Spawn the listening server
-    let server = TcpListener::bind(&addr)
-        .unwrap()
-        .incoming()
-        .for_each(move |conn| Ok(spawn_connection(conn, manager.clone())))
-        .map_err(|err| error!("Server Error: {:?}", err));
+    // Setup the cancellation
+    let device = server
+        .select2(cancel)
+        .map(move |_| { trace!("Closing device") })
+        .map_err(|_| { error!("Error during device closing") });
 
-
-    // If there is an ai server running (ie. a "parent server") connect to it
-    if let Some(paddr) = parent {
-        info!("Connecting to parent on {}", paddr);
-
-        let client = TcpStream::connect(&paddr)
-            .and_then(move |conn| Ok(spawn_connection(conn, ai_client)))
-            .map_err(|err| { error!("Client error: {:?}", err) });
-
-        let device = server
-            .join(client)
-            .select2(cancel)
-            .map(move |_| { trace!("Closing device") })
-            .map_err(|_err| { error!("Closing error") });
-
-        tokio::run(device);
-
-    // Otherwise, just start listening
-    } else {
-        info!("Initializing standalone device-manager");
-
-        let device = server
-            .select2(cancel)
-            .map(|_| { trace!("Closing device") })
-            .map_err(|_err| { error!("Closing error") });
-
-        tokio::run(device);
-    }
-
-    info!("System Shutdown");
+    // Run the futures within tokio
+    tokio::run(device);
+    info!("System shutdown");
 }
