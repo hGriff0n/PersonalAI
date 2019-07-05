a420e9f393ed66c6ffd1ca57c186b8e980b53b31
Grayson Hooper <ghooper96@gmail.com>
2019-07-05 18:14:10 -0500

    Create Test Client for RPC-based Rework
    Create initial test client for architecture rework to change the system to operate in a more rpc-based fashion.
    Also set up the rust server and provide an initial "schema" def file (although unused)

---
+++ experiments/rpc/client/client.py
@@ -0,0 +1,44 @@
+
+import json
+import socket
+import struct
+
+# TODO: Handle errors
+# TODO: Wrap in rpc interface
+# TODO: Make asynchronous?
+    # Apps should be able to "register_rpc" in the host somehow
+        # This would allow other apps to call those rpcs and those would be forwarded to the app
+        # The server handling code must be separate/asynchronous from the client code
+
+# Connect to server
+addr = "127.0.0.1:6142".split(':')
+sock = socket.socket()
+sock.connect((addr[0], int(addr[1])))
+
+# Construct rpc call
+# TODO: Wrap these in a client object (auto-generate?)
+msg = {
+    "call": "tell_fortune",
+    "args": {
+        "sign": "leo"
+    },
+    # "resp": None,
+    "msg_id": "foo",
+    "app_id": "me",
+    # TODO: Decide on other information to send as metadata
+        # How to identify specific locations (such as with speakers?)
+}
+
+# Send rpc to server
+data = json.dumps(msg).encode('utf-8')
+frame = struct.pack('>I', len(data))
+sock.sendall(frame + data)
+print("Send {} to server.....".format(msg))
+
+# Wait for response
+# TODO: Handle errors
+len_buf = sock.recv(4)
+msg_len = struct.unpack('>I', len_buf)[0]
+msg_buf = sock.recv(msg_len)
+msg = json.loads(msg_buf.decode('utf-8'))
+print("Received {}".format(msg))
