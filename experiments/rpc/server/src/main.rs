e16720136f00f5bc3af423d563f03b8d21c6feac
Grayson Hooper <ghooper96@gmail.com>
2019-07-06 17:54:41 -0500

    Add Signal to Close Communication
    Adds a oneshot channel that can be fired to close the client handles if desired

--- experiments/rpc/server/src/main.rs
+++ experiments/rpc/server/src/main.rs
@@ -69,12 +69,24 @@ fn serve(dispatcher: rpc::dispatch::Dispatcher, addr: std::net::SocketAddr) {
             // Construct communication channels between the read and write ends
             // This segments the control flow on the two ends, making the stream processing a little nicer
             let (sender, receiver) = futures::sync::mpsc::unbounded();
-            // TODO: Add in close channel? Closing it on the client's end automatically closes it on the server's end
+
+            // TODO: This is not how the original code handles this. Might want to change
+                // We can also keep the code in the read handler if chosen
+            let (signal, close_channel) = tokio::sync::oneshot::channel::<()>();
+            let mut signal = Some(signal);  // NOTE: This is required in order to allow for sending the signal into the read action
+
+            // TODO: Add in `register_new_connection` type callback?
 
             // Receive a message from the connection and dispatch it to the rpc service
             let rpc_dispatcher = dispatcher.clone();
             let read_action = reader
                 .for_each(move |msg| {
+                    // TODO: This is called for every request/response -> bad for app handling
+                        // The old code handled this by registering the 'connection' outside of this scope
+                        // Handler code would then access the "state" object to get the cancel signal
+                    // NOTE: This code is required in order to send the signal into the closure
+                    // signal.take().unwrap().send(()).unwrap();
+
                     // TODO: Figure out how to make this asynchronous?
                     // Marshal the call off to the rpc dispatcher
                     if let Some(msg) = rpc_dispatcher.dispatch(<P as protocol::RpcSerializer>::from_value(msg)?) {
@@ -100,9 +112,12 @@ fn serve(dispatcher: rpc::dispatch::Dispatcher, addr: std::net::SocketAddr) {
 
             // Spawn the actions in tokio
             let action = read_action
-                .select2(write_action)
+                .select(write_action)
+                .select2(close_channel)
+                // TODO: Add in `register_close_connection` type callback?
                 .map(move |_| println!("Closed connection with {:?}", addr))
                 .map_err(|_| eprintln!("close connection error"));
+
             tokio::spawn(action)
         });
 
